ef559f08d7b000cbfb964d1f13804c72ca64f169
fahmiLexso10 <fahmi.lexso.10@gmail.com>
2014-09-24 13:14:02 +0700

    updated

--- src/main/java/inc/john/belajar/model/Absent.java
+++ src/main/java/inc/john/belajar/model/Absent.java
@@ -1,22 +1,25 @@
 package inc.john.belajar.model;
 
 public class Absent {
-	
+
 	private Long id;
 	private Employee employee;
-	
+
 	/*
 	 * Getter setter.
 	 */
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public Employee getEmployee() {
 		return employee;
 	}
+
 	public void setEmployee(Employee employee) {
 		this.employee = employee;
 	}
